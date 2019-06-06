eec720f0d55888272378044115c14c2707c1925b
Nguyen Vo Hoang Duy <nguyenvohoangduy@gmail.com>
2019-06-06 22:15:30 +0700

    Finish section 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <nav className="navbar bg-primary">
-      <Navbar />
-    </nav>
-  );
+class App extends React.Component {
+
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({
+      loading: true
+    });
+
+    const res = await axios.get(`https://api.github.com/users?client_id=853e9fb025a8ada733ba&secret_id=5f7d87fcf5de1992e9e26eeb5ab93d8ab10c6ae3`);
+    
+    this.setState({
+      loading: false,
+      users: res.data
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+         <Users loading={this.state.loading} users={this.state.users}/>
+        </div>
+      </div>
+      
+    );
+  }
+  
 }
 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types';
-export default class Navbar extends Component {
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    }
+const Navbar =  ({icon, title}) => {
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>
-                    <i className={this.props.icon} /> {this.props.title}
-                </h1>
-            </div>
-        )
-    }
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+        </nav>
+    )
 }
+
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+}
+
+export default Navbar;
